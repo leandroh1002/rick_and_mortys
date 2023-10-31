1389571ae198cf7cf7d8e15526898aad90a079c3
leandroh1002 <leandroherrera1002@gmail.com>
2023-10-31 09:46:04 -0300

    login ok async await

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -40,6 +40,8 @@ function App() {
      const { email, password } = userData;
      const URL = 'http://localhost:3001/rickandmorty/login/';
     const {data} =await axios(URL + `?email=${email}&password=${password}`)
+    const {access} = data;
+
     setAccess(data);
        access && navigate('/home');
   } catch (error) {
