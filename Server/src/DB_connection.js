e0606ef0696a8249eee9b63454f2044d74dc93a8
leandroh1002 <leandroherrera1002@gmail.com>
2023-11-13 20:40:23 -0300

    updatte ignore

---
+++ Server/src/DB_connection.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+// EJERCICIO 03
+// A la instancia de Sequelize le falta la URL de conexión. ¡Agrégala!
+// Recuerda pasarle la información de tu archivo '.env'.
+
+// URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
+const sequelize = new Sequelize(
+   // URL
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+   { logging: false, native: false }
+);
+
+// EJERCICIO 05
+// Debajo de este comentario puedes ejecutar la función de los modelos.
+
+//
+
+//
+
+// Ejercicio 06
+// ¡Relaciona tus modelos aquí abajo!
+// const { User, Favorite } = sequelize.models;
+
+module.exports = {
+   // User,
+   // Favorite,
+   conn: sequelize,
+};
